264c3230464c26fc441c56b47a2dc9ffd5f140d6
Handerson Contreras <handerson.contreras@gmail.com>
2020-12-15 23:29:41 -0600

    removed unneed files

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -2,4 +2,4 @@ gcloud builds submit --tag=gcr.io/project-id/service --project=project-id
 
 gcloud run deploy service --image=gcr.io/project-id/service \
 --platform=managed --project=project-id --allow-unauthenticated --region=us-central1 \
---set-env-vars=OAUTH2_PROVIDER=https://liu-login-dev.appspot.com \
+--set-env-vars=OAUTH2_PROVIDER=https://liu-login-staging.appspot.com \
